715229f3537cbdbb0afee546113018f402b832b5
diki <adiki436@gmail.com>
2026-04-28 09:21:51 +0700

    initialize unit test

--- Ludo.Tests/obj/Debug/net8.0/Ludo.Tests.AssemblyInfo.cs
+++ Ludo.Tests/obj/Debug/net8.0/Ludo.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ludo.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b52d2d26d5e9888b8f421a6c98b0ac3724e849f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e37d4b271fc3b4904c5338a6f165f0b3b127779")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ludo.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ludo.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
